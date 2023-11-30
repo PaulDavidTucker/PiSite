b3fe908ad59a909ba7baaa15eb41526a6e6141fe
PaulDavidTucker <tuckerpauldavid@gmail.com>
2023-11-30 21:01:20 +0000

    Project setup and inital commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.getElementById("submitbtn").addEventListener("click", function() {
+    let email = document.getElementById("exampleInputEmail1").value;
+    document.getElementById("userinput").innerHTML = email;
+});
+
+
